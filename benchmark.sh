20877c2ed999a48ab0545fecbdf3daf77c10af60
ozansz <ozan.sazak@protonmail.ch>
2018-09-05 17:41:44 +0300

    Basic benchmarks

---
+++ benchmark.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo -e "[+] Installing dependencies...\n"
+sudo apt-get install python3-pip
+pip3 install requests
+
+echo -e "\n[i] Starting basic benchmark tests\n"
+
+echo -e "  => Running: time python3 -c \"import requests; requests.get('https://www.facebook.com')\""
+time python3 -c "import requests; requests.get('https://www.facebook.com')"
+
+echo -e "\n  => Running: time python3 -c \"import fastrequest; fastrequest.get('https://www.facebook.com')\""
+time python3 -c "import fastrequest; fastrequest.get('https://www.facebook.com')"
+
+echo -e "\n[i] All tests run. Bye!"
