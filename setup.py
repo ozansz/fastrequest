f795ab77e8cfdc48f13a2e130fc04fa42af25fbf
ozansz <ozan.sazak@protonmail.ch>
2018-11-07 11:06:16 +0300

    new project structure is created

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import Extension, setup
 
-module = Extension("fastrequest", sources=['fastrequest.c', 'libfrext.c'],
+module = Extension("fastrequest", sources=['src/fastrequest.c', 'src/libfrext.c'],
     library_dirs=['/usr/local/lib'], libraries=['curl'])
 
 setup(
     name="fastrequest",
-    version="0.4-beta",
+    version="0.5-beta",
     description="fastrequest fast http requests extension",
     ext_modules=[module]
 )
