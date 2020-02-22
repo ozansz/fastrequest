921218e5483d973da02b67043512d877033aecd8
Ozan Sazak <ozan.sazak@protonmail.ch>
2020-02-22 14:03:12 +0300

    added version check

--- src/models.h
+++ src/models.h
@@ -4,6 +4,10 @@
 
 #define PY_SSIZE_T_CLEAN
 
+#if PY_MAJOR_VERSION < 3
+    #error This library is compatible for Python version >= 3 only!
+#endif
+
 typedef struct {
     PyObject_HEAD
     PyObject *response_size;
