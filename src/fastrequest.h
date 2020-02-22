85a112749e612c59cbccd556ca4c96442ac30952
Ozan Sazak <ozan.sazak@protonmail.ch>
2020-02-22 14:07:35 +0300

    added platform check directives and moved to fastrequest.h

--- src/fastrequest.h
+++ src/fastrequest.h
@@ -4,4 +4,14 @@
 
 #undef FR_DEBUG
 
+#if PY_MAJOR_VERSION < 3
+    #error This library is compatible for Python version >= 3 only!
+#endif
+
+#ifdef __GNUC__
+    #ifdef _WIN32
+        #error This library is compatible for Linux-based platforms only!
+    #endif
+#endif
+
 PyObject *FastRequest_HttpGet(PyObject *self, PyObject *args);
