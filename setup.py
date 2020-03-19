663e0fc7cc5c0a4121d5a4e7789a421bde5297f0
Ozan Sazak <ozan.sazak@protonmail.ch>
2020-03-19 17:05:08 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -15,10 +15,24 @@ http_module = Extension(
     libraries=['curl']
 )
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="fastrequest",
-    version="1.6.1-beta",
+    version="1.6.3-beta",
+    author="Ozan Sazak",
+    author_email="ozan.sazak@protonmail.ch",
     description="fastrequest fast http requests extension",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ozansz/fastrequest",
     packages=['fastrequest'],
-    ext_modules=[http_module]
+    ext_modules=[http_module],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
 )
